0c5c447b50c54f76abbb939b674fed2b7895fcb6
Otoniel Reyes <kenliten@otonielreyes.com>
2022-10-11 19:08:23 -0400

    Added auth components

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { PasswordRecoveryComponent } from './password-recovery/password-recovery.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    SignupComponent,
+    PasswordRecoveryComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
